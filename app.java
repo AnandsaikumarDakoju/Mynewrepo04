4de086c1153979a704c278db1909e72ed0e07e46
root <root@ip-172-31-71-175.ec2.internal>
2025-11-24 06:59:15 +0000

    java code commited

---
+++ app.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Hello, GitHub!");
+        
+        // Show current date and time
+        java.time.LocalDateTime now = java.time.LocalDateTime.now();
+        System.out.println("Current Date and Time: " + now);
+        
+        // Simple addition
+        int a = 5, b = 10;
+        System.out.println("Sum of " + a + " and " + b + " is: " + (a + b));
+    }
+}
